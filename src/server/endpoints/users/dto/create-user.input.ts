b6419f4f4b1076ed78da7384e65d122c9cc7dc7f
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-01-13 19:33:29 +0100

    Validation for create user input

--- src/server/endpoints/users/dto/create-user.input.ts
+++ src/server/endpoints/users/dto/create-user.input.ts
@@ -1,9 +1,21 @@
 import { InputType, Field } from '@nestjs/graphql';
 
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+
+import { Gender } from '../../../common/enums/gender.enum';
 import { BaseEndpointInput } from '../../../common/inputs/base-endpoint.input';
 
 @InputType()
 export class CreateUserInput extends BaseEndpointInput {
-  @Field()
-  public name: string;
+  @IsEmail()
+  @Field(() => String)
+  public email: string;
+
+  @IsNotEmpty()
+  @Field(() => String)
+  public password: string;
+
+  @IsEnum(Gender)
+  @Field(() => Gender)
+  public gender: Gender;
 }
