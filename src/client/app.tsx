d7837a1df414967b32e52ee5415b4652164f7b12
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-01-28 13:56:33 +0100

    Add MUI theme provider

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -3,11 +3,16 @@ import { Route, Routes } from 'react-router';
 
 import { Layout } from './layout';
 import { HomePage } from './pages/home.page';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({ cssVariables: { cssVarPrefix: '' } });
 
 export const App = (): ReactElement => (
-  <Routes>
-    <Route element={<Layout/>}>
-      <Route path='/' element={<HomePage />} />
-    </Route>
-  </Routes>
+  <ThemeProvider theme={theme}>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path='/' element={<HomePage />} />
+      </Route>
+    </Routes>
+  </ThemeProvider>
 );
