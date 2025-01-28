2cd0bbfb2a79588b31e67c27637d1463191eb15b
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-01-28 13:39:14 +0100

    Add eslint and prettier support for React

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 import lint from 'typescript-eslint';
 
@@ -8,9 +8,10 @@ export default lint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
-  eslint.configs.recommended,
+  lint.configs.recommended,
   ...lint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  eslintPluginReact,
   {
     languageOptions: {
       globals: {
