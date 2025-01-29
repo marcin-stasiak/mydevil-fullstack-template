13a43846cae00d5e15f1ec93c58493de068983d5
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-01-29 10:57:08 +0100

    Update build script command

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ export CXX=g++
 echo "Install modules..."
 npm22 install --prefix "$(dirname "$0")"
 
-echo "Build client application..."
-npm22 run build:client --prefix "$(dirname "$0")"
-
-echo "Build client application..."
-npm22 run build:server --prefix "$(dirname "$0")"
+echo "Build application..."
+npm22 run build --prefix "$(dirname "$0")"
