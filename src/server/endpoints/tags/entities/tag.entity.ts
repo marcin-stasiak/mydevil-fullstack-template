db6553aafc1011fe59843683d072e1a656515e0e
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-02-01 16:44:32 +0100

    Add gql query files

--- src/server/endpoints/tags/entities/tag.entity.ts
+++ src/server/endpoints/tags/entities/tag.entity.ts
@@ -1,6 +1,6 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 
-import { Entity, ManyToMany } from 'typeorm';
+import { Column, Entity, ManyToMany } from 'typeorm';
 
 import { BaseEndpointEntity } from '../../../common/entities/base-endpoint.entity';
 import { Entry } from '../../entries/entities/entry.entity';
@@ -8,6 +8,10 @@ import { Entry } from '../../entries/entities/entry.entity';
 @ObjectType()
 @Entity('tags')
 export class Tag extends BaseEndpointEntity {
+  @Field(() => String)
+  @Column()
+  public title: string;
+
   @Field(() => [Entry])
   @ManyToMany(() => Entry, (entry) => entry.tags)
   public entries: Entry[];
