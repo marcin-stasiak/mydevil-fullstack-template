b4cc403bcb7c0b01ac1635cc7864a049e287dee1
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-02-14 21:35:54 +0100

    Update example client components

--- src/client/components/footer.tsx
+++ src/client/components/footer.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, useTheme } from '@mui/material';
 
 export const Footer = () => {
+  const theme = useTheme();
+
   return (
-    <Box component='footer'>
+    <Box
+      component='footer'
+      sx={{ color: theme.palette.background.default, backgroundColor: theme.palette.primary.main }}
+    >
       <Container>
-        <Typography component='small'>© 2025, All Rights Reserved</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
+          <Typography component='small' sx={{ fontSize: '14px', fontWeight: '200' }}>
+            © My Devil 2025, All Rights Reserved
+          </Typography>
+        </Box>
       </Container>
     </Box>
   );
--- src/client/components/header.tsx
+++ src/client/components/header.tsx
@@ -1,39 +1,67 @@
 import React from 'react';
-import { AppBar, Box, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, ButtonGroup, Container, Drawer, IconButton, Link, Toolbar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useQuery } from '@apollo/client';
+import { SETTING_QUERY } from '../common/graphql/setting.query';
 
 export const Header = () => {
+  const { data: menuMain } = useQuery(SETTING_QUERY, {
+    variables: {
+      name: 'menu_main',
+    },
+    fetchPolicy: 'cache-first',
+  });
+
   return (
-    <AppBar position='sticky'>
-      <Container>
-        <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ display: { xs: 'flex', md: 'none' }, mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            noWrap
-            component='a'
-            href='/'
-            sx={{
-              color: 'inherit',
-              fontStyle: '20px',
-              fontWeight: '700',
-              textDecoration: 'none',
-              textTransform: 'uppercase',
-              mr: 2,
-            }}
-          >
-            My Devils
-          </Typography>
-          <Box sx={{ flexGrow: 1 }}>Link Link</Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <>
+      <AppBar position='sticky'>
+        <Container>
+          <Toolbar>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+              sx={{ display: { xs: 'flex', md: 'none' }, mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Link
+              noWrap
+              component='a'
+              href='/'
+              sx={{
+                display: 'flex',
+                flexGrow: { sx: 1, md: 0 },
+                color: 'inherit',
+                fontStyle: '22px',
+                fontWeight: '700',
+                textDecoration: 'none',
+                textTransform: 'uppercase',
+                mr: 2,
+              }}
+            >
+              My Devils
+            </Link>
+            <Box component='nav' sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1, justifyContent: 'center' }}>
+              {menuMain?.setting?.value?.map((item: { link: string; content: string }) => (
+                <Link key={item.link} href={item.link} sx={{ color: 'inherit', mx: 1 }}>
+                  {item.content}
+                </Link>
+              ))}
+            </Box>
+            <ButtonGroup variant='text' aria-label='Login and register'>
+              <Button href='/login' sx={{ color: 'inherit' }}>
+                Login
+              </Button>
+              <Button href='/register' sx={{ color: 'inherit' }}>
+                Register
+              </Button>
+            </ButtonGroup>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer>dd</Drawer>
+    </>
   );
 };
