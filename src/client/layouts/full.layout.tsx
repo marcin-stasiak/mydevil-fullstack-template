d5b12db245b8cf6d4ae65cfcee15d023ca90d55a
Marcin Stasiak <kontakt@marcinstasiak.pl>
2025-02-18 13:10:57 +0100

    Update TODO's and remove unused imports

--- src/client/layouts/full.layout.tsx
+++ src/client/layouts/full.layout.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Outlet } from 'react-router';
 import { Box } from '@mui/material';
 
